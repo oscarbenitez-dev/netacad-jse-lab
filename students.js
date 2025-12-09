344e3f7e4c90c9bad4a54e85d48094fa4af23652
Oscar Benítez <contacto@oscarbenitez.dev>
2025-12-09 22:19:32 +0100

    2.0.15 Variables

---
+++ students.js
@@ -0,0 +1,13 @@
+/* Declare and initialize the variables where you will store all the information (nine variables in total). Display in the console information about the first and last contact in the form: name/phone/email. */
+let name01 = "Maxwell Wright";
+let name02 = "Raja Villarreal";
+let name03 = "Helen Richards";
+let phone01 = "(0191)7196495";
+let phone02 = "08663982895";
+let phone03 = "08001111";
+let email01 = "Curabitur.egestas.nun@nonummyac.co.uk";
+let email02 = "posuere.vulputate@sed.com";
+let email03 = "libero@convallis.edu";
+
+console.log(`${name01} / ${phone01} / ${email01}`);
+console.log(`${name03} / ${phone03} / ${email03}`);
